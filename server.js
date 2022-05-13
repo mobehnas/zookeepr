fd6b16a92e6558b1627cddfb3d811c5ae939616a
mohamed behnas <behna@DESKTOP-T1K4JBI.(none)>
2022-05-13 16:30:20 -0400

    Set Up Express.js Server and GET Routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const res = require('express/lib/response');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3006;
 const app = express();
 const { animals } = require('./data/animals');
 
@@ -42,12 +42,26 @@ function filterByQuery(query, animalsArray) {
   // return the filtered results:
   return filteredResults;
 }
+
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
   }
   res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
 
 app.listen(PORT, () => {
